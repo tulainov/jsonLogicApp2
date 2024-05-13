77ef4d3967da0292795f3b31fcf0966cc2f2116c
Danylo Tulainov <danylo.tulainov@voith.com>
2024-05-13 16:50:03 +0200

    today was a complete time waste. Wanted to integrate the code from jsonlogic web-site but couldn't do it. have an idea for tomorrow. I'm going to do everything just with jsonLogic lib, will create two input fields. Simply - Rule and Data. Will create a link to go to the jsonLogic web-site, simple html, simple css. One output field for result of an operation. and output of every decision made by user, in a json format.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as jsonLogic from 'json-logic-js'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'jsonLogicApp2';
+
+  rule: any;
+  data: any;
+
+  getRuleAndData() {
+
+  }
+
+
+  // result: any = jsonLogic.apply(this.rule, this.data);
 }
