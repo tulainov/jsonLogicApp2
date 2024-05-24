c85707da8b9fc8a5009f41acbc9f78d66ced4ded
Danylo Tulainov <danylo.tulainov@voith.com>
2024-05-24 13:55:30 +0200

    nodes are displayed now. next step - perform the AND or OR operation between nodes

--- src/app/tree-and-logic/tree-and-logic.component.ts
+++ src/app/tree-and-logic/tree-and-logic.component.ts
@@ -1,35 +1,87 @@
 import { Component } from '@angular/core';
-import * as jsonLogic from 'json-logic-js'
-import {FormBuilder, FormArray, Validators } from '@angular/forms';
+import * as jsonLogic from 'json-logic-js';
+import { FormBuilder, FormArray, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-tree-and-logic',
-  templateUrl: './tree-and-logic.component.html',
-  styleUrls: ['./tree-and-logic.component.css']
+    selector: 'app-tree-and-logic',
+    templateUrl: './tree-and-logic.component.html',
+    styleUrls: ['./tree-and-logic.component.css']
 })
-//TODO: use JSON Pipe to represent the logic in the end
 export class TreeAndLogicComponent {
 
-  constructor(private formBuilder: FormBuilder) {
-  }
+    operation: any;
+    val: any;
+    result: any;
 
-  logic = this.formBuilder.group({
-      // TODO: condition split with split() add all operations, with switch case go though them, if equals something - then we'll be using it, if no - throw an error
-      data: ['', Validators.required],
-      condition: ['', Validators.required],
+    constructor(private formBuilder: FormBuilder) {}
 
-      nodes: this.formBuilder.array([this.formBuilder.control('')])
-  });
-
-  get nodes() {
-    return this.logic.get('nodes') as FormArray;
-  }
-
-  addNode() {
-    const node = this.formBuilder.group({
+    logic = this.formBuilder.group({
         data: ['', Validators.required],
-        condition: ['', Validators.required]
+        condition: ['', Validators.required],
+        logicalOperator: ['', Validators.required],
+        nodes: this.formBuilder.array([])
     });
-    this.nodes.push(node);
-  }
+
+    get nodes() {
+        return this.logic.get('nodes') as FormArray;
+    }
+
+    get condition() {
+        return this.logic.get('condition');
+    }
+
+    get data() {
+        return this.logic.get('data');
+    }
+
+    get logicalOperator() {
+        return this.logic.get('logicalOperator');
+    }
+
+    addNode() {
+        const node = this.formBuilder.group({
+            data: ['', Validators.required],
+            condition: ['', Validators.required]
+        });
+        this.nodes.push(node);
+    }
+
+    convertOpAndVal() {
+        const cond = this.condition?.value as string;
+
+        if (!cond) {
+            throw new Error('Condition is required');
+        }
+
+        const parts = cond.split(' ');
+
+        if (parts.length === 2) {
+            this.operation = parts[0];
+            this.val = parseFloat(parts[1]); // Ensure the value is treated as a number
+        } else {
+            throw new Error('Invalid condition format');
+        }
+    }
+
+    applyLogic() {
+        this.convertOpAndVal();
+
+        const dataValue = parseFloat(this.data?.value as string); // Ensure the data is treated as a number
+
+        if (isNaN(dataValue)) {
+            throw new Error('Data must be a valid number');
+        }
+
+        // Construct JSON Logic rule
+        const rule = {
+            [this.operation]: [dataValue, this.val]
+        };
+
+        // Apply JSON Logic
+        this.result = jsonLogic.apply(rule, {});
+    }
+
+    refreshPage() {
+        window.location.reload();
+    }
 }
